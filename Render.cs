d5ff4cc3873edeaf090e59fd1f92b6aff2635c08
arduano <leonid.shchurov@gmail.com>
2019-03-15 00:15:27 +1100

    memory leaks

--- Render.cs
+++ Render.cs
@@ -119,6 +119,9 @@ void main()
         {
             GL.DeleteBuffers(3, new int[] { vertexBufferID, colorBufferID, attribBufferID });
             GL.DeleteProgram(noteShader);
+            quadVertexbuff = null;
+            quadColorbuff = null;
+            quadAttribbuff = null;
             Initialized = false;
             Console.WriteLine("Disposed of ClassicRender");
         }
